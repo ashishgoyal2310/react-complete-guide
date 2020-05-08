bd3b4fed4b82da5a11c68e26e30d2fa74cf853e5
Ashish Goyal <ashish2.goyal@orange.com>
2020-05-08 09:16:15 +0530

    Update for Navigation item new component

--- src/components/Navigation/NavigationItems.js
+++ src/components/Navigation/NavigationItems.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import classes from './Navigation.module.css'
 
+const NavigationItem = (props) => (
+    <li className={props.active ? classes.active : null}><a href="{props.link}">{props.txt}</a></li>
+);
+
 const NavigationItems = (props) => {
     return (
         <ul>
-            <li className={props.active ? classes.active : null}><a href="/">Create Burger</a></li>
-            <li><a href="/">Checkout</a></li>
+            <NavigationItem link='/' txt='Build Burger' active />
+            <NavigationItem link='/' txt='Checkout' />
         </ul>
     );
 }
