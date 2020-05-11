0512ab14d6f48a1db0e1f3aff8bbefe3a7adc2c8
Ashish Goyal <ashish2.goyal@orange.com>
2020-05-11 10:23:01 +0530

    Handling Errors Locally

--- src/containers/Blog/index.js
+++ src/containers/Blog/index.js
@@ -7,13 +7,20 @@ import PostCreate from '../../components/FullPost/PostCreate'
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostid: null
+        selectedPostid: null,
+        error: false
     }
 
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        const rnd = Math.random();
+        let url = 'https://jsonplaceholder.typicode.com/posts';
+        if (rnd > 0.5) { // Randomize to use wrong url to check error section
+            url = 'https://jsonplaceholder.typicode.com/postss';
+        }
+
+        axios.get(url)
             .then(response => {
-                // console.log(response.data);
+                console.log(response);
                 const posts = response.data.slice(0, 5);
                 const updatePosts = posts.map(post => {
                     return { ...post, author: 'Ashish Goyal'}
@@ -21,6 +28,9 @@ class Blog extends Component {
                 this.setState({
                     posts: updatePosts
                 });
+            })
+            .catch(response => {
+                this.setState({ error: true });
             });
     }
 
@@ -38,14 +48,19 @@ class Blog extends Component {
     }
 
     render() {
-        const postsListing = this.state.posts.map(post => {
-            return <PostList
-                        key={post.id}
-                        id={post.id}
-                        title={post.title}
-                        author={post.author}
-                        clicked={() => this.selectPostHandler(post.id)} />;
-        });
+        let postsListing = <p>Loading...</p>;
+        if (this.state.error && !this.state.posts.length) {
+            postsListing = <p>Error while loading...</p>;
+        } else {
+            postsListing = this.state.posts.map(post => {
+                return <PostList
+                            key={post.id}
+                            id={post.id}
+                            title={post.title}
+                            author={post.author}
+                            clicked={() => this.selectPostHandler(post.id)} />;
+            });
+        }
 
         return (
             <React.Fragment>
