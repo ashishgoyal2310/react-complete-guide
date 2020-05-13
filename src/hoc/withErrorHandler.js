73d00c6d203c461b151128c3c2c5071a41dc887c
Ashish Goyal <ashish2.goyal@orange.com>
2020-05-13 17:28:34 +0530

    10 - Remove old axios interceptos

--- src/hoc/withErrorHandler.js
+++ src/hoc/withErrorHandler.js
@@ -8,22 +8,29 @@ const withErorHandler = ( WrapperComponent, axios ) => {
     return class extends Component {
         state = {
             error: ''
-        };
+        }
 
         componentWillMount() {
-            axios.interceptors.request.use(req => {
+            this.requestInterceptos = axios.interceptors.request.use(req => {
                 console.log('[withErorHandler.js] componentWilMount => axios.interceptors.request', req);
                 this.setState({ error: '' })
                 return req;
-            })
-            axios.interceptors.response.use(response => {
+            });
+
+            this.responseInterceptor = axios.interceptors.response.use(response => {
                 console.log('[withErorHandler.js] componentWilMount => axios.interceptors.response', response);
                 return response;
             }, error => {
                 console.log('[withErorHandler.js] componentWilMount => axios.interceptors.error', error);
                 this.setState({ error: error })
                 // return error;
-            })
+            });
+        }
+
+        componentWillUnmount() {
+            console.log('[withErorHandler.js] componentWillUnmount ', this.requestInterceptos, this.responseInterceptor);
+            axios.interceptors.request.eject(this.requestInterceptos);
+            axios.interceptors.response.eject(this.responseInterceptor);
         }
 
         render() {
