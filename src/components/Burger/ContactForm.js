032ab8882ff5668ac62acf10d98185fd2fd6748c
Ashish Goyal <ashish2.goyal@orange.com>
2020-05-29 12:38:50 +0530

    Update to create form infput in dynamic manner

--- src/components/Burger/ContactForm.js
+++ src/components/Burger/ContactForm.js
@@ -1,20 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../UI/Button'
 import FormInput from '../UI/FormInput'
 import classes from './Burger.module.css'
 
-const ContactForm = (props) => {
-    return (
-        <div>
-            <form className={ classes.ContactFrom }>
-                <FormInput />
-                <FormInput />
-                <FormInput />
-                <FormInput />
-                <Button btnType="Success" clicked={props.makeOrder}>Make Order</Button>
-            </form>
-        </div>
-    );
+class ContactForm extends Component {
+    state = {
+        formElements: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: "text",
+                    placeholder: "Name"
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: "email",
+                    placeholder: "Email"
+                },
+                value: ''
+            },
+            instruction: {
+                elementType: 'textarea',
+                elementConfig: {
+                    type: "text",
+                    placeholder: "Any Instruction"
+                },
+                value: ''
+            },
+            deliveryType: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: "normal", displayValue: "Normal"},
+                        {value: "fastest", displayValue: "Fastest"},
+                        {value: "cheapest", displayValue: "Cheapest"},
+                    ]
+                },
+                value: ''
+            }
+        }
+    }
+
+    render() {
+        const formElementLst = []
+        for (let key in this.state.formElements) {
+            const eleData = this.state.formElements[key]
+            formElementLst.push(
+                <FormInput
+                    key={key}
+                    elementType={eleData.elementType}
+                    config={eleData.elementConfig}
+                    value={eleData.value} />
+            );
+        }
+
+        return (
+            <div>
+                <form className={ classes.ContactFrom }>
+                    { formElementLst }
+                    <Button btnType="Success" clicked={this.props.makeOrder}>Make Order</Button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default ContactForm;
--- src/components/UI/FormInput/index.js
+++ src/components/UI/FormInput/index.js
@@ -3,11 +3,34 @@ import React from 'react';
 import classes from './FormInput.module.css'
 
 const FormInput = (props) => {
-    return (
-        <input
-            className={ classes.FormInput }
-            type="text" />
-    );
+    let inputElement = null;
+    switch ( props.elementType ) {
+        case ('textarea'):
+            inputElement = <textarea
+                                className={ classes.FormInput }
+                                {...props.config}
+                                value={ props.value }>
+                            </textarea>
+            break;
+        case ('select'):
+            inputElement = <select
+                                className={ classes.FormInput }
+                                value={ props.value }>
+                                <option value="">---</option>
+                                {props.config.options.map(option => (
+                                    <option key={option.value} value={option.value}>{option.displayValue}</option>
+                                ))}
+                            </select>
+            break;
+        default:
+            inputElement = <input
+                            className={ classes.FormInput }
+                            {...props.config}
+                            value={ props.value } />;
+            break;
+    }
+
+    return inputElement;
 }
 
 export default FormInput;
